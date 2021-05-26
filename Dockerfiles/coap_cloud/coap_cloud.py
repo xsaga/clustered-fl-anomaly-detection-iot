66669579ce045bf1f1edce742decb3ba059cb207
Xabier SdC <xabiersdc@gmail.com>
2021-05-26 11:07:41 +0200

    Use IP address ranges in COAP_ADDR_LIST var.

--- Dockerfiles/coap_cloud/coap_cloud.py
+++ Dockerfiles/coap_cloud/coap_cloud.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import ipaddress
 import os
 import random
 import sys
@@ -7,6 +8,18 @@ import shlex
 import shutil
 import subprocess
 import time
+from typing import List
+
+
+def iprange(start_addr: str, end_addr: str) -> List[ipaddress.IPv4Address]:
+    start = int(ipaddress.IPv4Address(start_addr))
+    end = int(ipaddress.IPv4Address(end_addr))
+    assert start <= end
+    addresses = []
+    for i in range(start, end+1):
+        addresses.append(ipaddress.IPv4Address(i))
+    return addresses
+
 
 config = {"COAP_ADDR_LIST": "",
           "SLEEP_TIME": 5,
@@ -18,7 +31,12 @@ for key in config.keys():
     except KeyError:
         pass
 
-config["COAP_ADDR_LIST"] = list(map(str.strip, config["COAP_ADDR_LIST"].split(";")))
+address_list = []
+# config["COAP_ADDR_LIST"] example: "192.168.10.1-192.168.10.50;192.168.20.2-192.168.20.2;192.168.30.0-192.168.30.5"
+for ip_range in list(map(str.strip, config["COAP_ADDR_LIST"].split(";"))):
+    if ip_range:
+        address_list.extend(iprange(*list(map(str.strip, ip_range.split("-")))))
+config["COAP_ADDR_LIST"] = address_list
 
 coap_bin = shutil.which("coap-client")
 if not coap_bin:
